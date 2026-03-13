7bff7a1d75adbc8dbbe41cdd78b7ef37d1a9caa5
Swarup Das <swarupdas2046@gmail.com>
2026-03-13 14:42:59 +0530

    some changes

--- practice-1/src/Components/Card.jsx
+++ practice-1/src/Components/Card.jsx
@@ -9,7 +9,7 @@ const Card = (elem) => {
       <h1 className="text-2xl font-semibold ">{elem.productName}</h1>
       <p className="text-xl font-light"> {elem.category} </p>
       <h3 className="text-green-600 font-bold text-xl">₹{elem.price} </h3>
-      <button className="px-5 py-2.5 mb-3 text-white rounded border-none font-medium bg-[#007BFF]">
+      <button className="px-4 py-2 mb-3 text-white rounded border-none font-medium bg-[#007BFF] hover:bg-[#0056B3]">
         Add to cart
       </button>
     </div>
